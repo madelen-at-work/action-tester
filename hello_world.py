1a7d23206b8a0973b26387de7b4ecbdb37254756
Madelen Andersson <madelan@axis.com>
2024-03-30 13:33:28 +0100

    slightly bigger change

--- hello_world.py
+++ hello_world.py
@@ -4,10 +4,10 @@ import datetime
 
 DEFAULT_NAME="action-tester"
 
-def main(name):
+def my_function(name):
     ''' Prints a greeting and the current time. '''
     print(f"Hello world! from {name}."
           f" The time is {datetime.datetime.now()}")
 
 if __name__ == "__main__":
-    main(DEFAULT_NAME)
+    my_function(DEFAULT_NAME)
