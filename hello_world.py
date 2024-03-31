4e14df24dd77b4d16360fe00cb7afad9700d5791
Madelen Andersson <madelan@axis.com>
2024-03-31 14:40:08 +0200

    Changed formatting (#18)
    needed some space.

--- hello_world.py
+++ hello_world.py
@@ -6,6 +6,7 @@ DEFAULT_NAME="action-tester"
 
 def my_function(name):
     ''' Prints a greeting and the current time. '''
+
     print(f"Hello world! from {name}."
           f" The time is {datetime.datetime.now()}")
 
